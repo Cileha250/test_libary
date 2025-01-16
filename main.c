f69c23cd6a3dbad479009d91f21129b7e597cce1
Cileha250 <estherkamto@gmail.com>
2025-01-16 07:52:39 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int result = power( 2, 3 );
+ printf( "2³ == %d\n", result );
+ result = fact( 6 );
+ printf( "6! == %d\n", result );
+ return 0;
+}
